3a6a17775f47e476496845af49dfdde3d9e74d41
Callum Hopkins <callum@recruitmentbot.co.uk>
2018-10-13 12:52:38 +0100

    updates

--- server.js
+++ server.js
@@ -58,6 +58,57 @@ app.get('/view/:id', async (req, res) => {
     }
 });
 
+app.get('/update/:id', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT id,name,msg,tags,twitter,status FROM helpnotice where id = $1', [req.params.id]);
+        const notice = (result) ? result.rows : null;
+        client.release();        
+        res.render('update.html', {notice:notice, issue: _.unescape(notice[0].msg), tags:JSON.parse(notice[0].tags), error:false});
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
+app.post('/update/:id', async (req, res) => {
+    const results = [];
+    const data = req.body;
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT passcode FROM helpnotice where id = $1', [data.id]);
+        const notice = (result) ? result.rows : null;
+
+        let passcode = crypto.createHmac('sha256',secret)
+            .update(data.passcode)
+            .digest('hex');
+
+        let msg = _.escape(data.msg);
+        let tags = JSON.stringify(data.tags);
+
+
+        if(notice[0].passcode === passcode){
+            try {
+                const client = await pool.connect()
+                let insert = await client.query('update helpnotice set name=$1, msg=$2, status=$3, twitter=$4, tags =$5 WHERE id=$6',[data.name, msg, data.status, data.twitter, tags, data.id]);
+                client.release();
+                res.redirect('/');
+            } catch (err) {
+                console.error(err);
+                res.send("Error " + err);
+            }      
+        }else{
+            console.error('passcode not match');
+            res.send("Error: passcode not match did not update ");         
+        }
+
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
 app.post('/submit', async (req, res) => {
     const results = [];
     // Grab data from http request
@@ -72,7 +123,6 @@ app.post('/submit', async (req, res) => {
 
     let tags = JSON.stringify(data.tags);
 
-    //if(!req.body.){
         try {
             const client = await pool.connect()
             let insert = await client.query('INSERT INTO helpnotice(name, passcode, msg, status, twitter, tags) values($1, $2, $3, $4, $5, $6)',[data.name, passcode, msg, data.status, data.twitter, tags]);
@@ -82,18 +132,9 @@ app.post('/submit', async (req, res) => {
             console.error(err);
             res.send("Error " + err);
         }
-    /*}else{
-        try {
-            const client = await pool.connect()
-            let insert = await client.query('UPDATE attendee set card=$1 where id = $2',[data.card,data.index]);
-            client.release();
-            res.redirect('/');
-        } catch (err) {
-            console.error(err);
-            res.send("Error " + err);
-        } 
-    }*/
 });
+
+
 
 app.get('/', async (req, res) => {
     try {
